502e42a4f93fd68ac1914d88463a25e0dd2f35c2
Hari-42 <harisaranmohanathas@hotmail.com>
2026-06-29 14:05:27 +0200

    Aufgabe 2.2.2: vociList im Learn Screen bereitstellen

--- src/app/learn.tsx
+++ src/app/learn.tsx
@@ -1,4 +1,16 @@
 import { View, Text, StyleSheet } from 'react-native';
+import Voci from '@/models/voci';
+
+const vociList: Voci[] = [
+  { term: 'apple', translation: 'Apfel' },
+  { term: 'house', translation: 'Haus' },
+  { term: 'car', translation: 'Auto' },
+  { term: 'book', translation: 'Buch' },
+  { term: 'water', translation: 'Wasser' },
+  { term: 'sun', translation: 'Sonne' },
+  { term: 'dog', translation: 'Hund' },
+  { term: 'cat', translation: 'Katze' },
+];
 
 export default function LearnScreen() {
   return (
