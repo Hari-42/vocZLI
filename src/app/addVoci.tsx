265d940e46148b717e11adc9faf58956f9e8c7c7
Hari-42 <harisaranmohanathas@hotmail.com>
2026-06-30 08:30:27 +0200

    Aufgabe 2.4.2: Add Screen erstellen

---
+++ src/app/addVoci.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from 'expo-router';
+import VociDetail from '@/components/VociDetail';
+import { useVoci } from '@/context/vociContext';
+import Voci from '@/models/voci';
+
+export default function AddVociScreen() {
+  const router = useRouter();
+  const { addVoci } = useVoci();
+
+  function handleAdd(newVoci: Voci) {
+    addVoci(newVoci);
+    router.back();
+  }
+
+  return <VociDetail onSave={handleAdd} />;
+}
