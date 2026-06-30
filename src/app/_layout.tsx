c65dd40fe7426805f301323efca36120459b8296
Hari-42 <harisaranmohanathas@hotmail.com>
2026-06-30 08:24:31 +0200

    Aufgabe 2.3.2: VociProvider in App-Hierarchie einbinden

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,30 +1,33 @@
 import { Stack } from 'expo-router';
+import { VociProvider } from '../context/vociContext';
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#005380',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          title: 'Meine Vokabeln',
+    <VociProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#005380',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
         }}
-      />
-      <Stack.Screen
-        name="learn"
-        options={{
-          title: 'Vokabeln lernen',
-        }}
-      />
-    </Stack>
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'Meine Vokabeln',
+          }}
+        />
+        <Stack.Screen
+          name="learn"
+          options={{
+            title: 'Vokabeln lernen',
+          }}
+        />
+      </Stack>
+    </VociProvider>
   );
 }
